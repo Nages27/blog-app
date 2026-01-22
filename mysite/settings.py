19e3d90cd703a46f575e42da4da7f77af382c86c
Nageswari N <youremail@example.com>
2026-01-22 19:56:34 +0530

    Updated CSS, added dashboard background, and fixed templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eq0ugb+@7if)r*c2nkmsubdhq9x8prb68(%t4*ad124z+@pskr'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myblogdb',
-        'USER': 'mybloguser',
-        'PASSWORD': 'Nages@27',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': config("NAME"),
+        'USER': config("mybloguser"),
+        'PASSWORD': config('Nages@27'),
+        'HOST': ('localhost'),
+        'PORT': config('3306'),
     }
 }
 
